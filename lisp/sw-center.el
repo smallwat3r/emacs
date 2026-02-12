74005d8b2a5e0b7a2f6e2fdd3d2cbcfa8690a13f
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2026-02-12 20:39:53 +0000

    Fmt

--- lisp/sw-center.el
+++ lisp/sw-center.el
@@ -13,21 +13,17 @@
   "Return non-nil if window tree NODE contains a horizontal split."
   (when (listp node)
     (or (not (car node))
-        (cl-some #'sw-center--has-hsplit-p
-                 (cddr node)))))
+        (cl-some #'sw-center--has-hsplit-p (cddr node)))))
 
 (defun sw-center--update (&rest _)
   "Update centering margins on all windows."
-  (let ((squash (sw-center--has-hsplit-p
-                 (car (window-tree)))))
+  (let ((squash (sw-center--has-hsplit-p (car (window-tree)))))
     (walk-windows
      (lambda (w)
        (set-window-margins w 0 0)
        (let* ((target (or sw-center-width fill-column 100))
               (available (window-body-width w))
-              (margin (if squash 0
-                        (max 0 (/ (- available target)
-                                  2)))))
+              (margin (if squash 0 (max 0 (/ (- available target) 2)))))
          (set-window-margins w margin margin)))
      'no-mini)))
 
@@ -43,11 +39,9 @@ Margins are removed when the frame has side-by-side windows."
   :global t :lighter nil
   (if sw-center-mode
       (progn
-        (add-hook 'window-configuration-change-hook
-                  #'sw-center--update)
+        (add-hook 'window-configuration-change-hook #'sw-center--update)
         (sw-center--update))
-    (remove-hook 'window-configuration-change-hook
-                 #'sw-center--update)
+    (remove-hook 'window-configuration-change-hook #'sw-center--update)
     (sw-center--clear)))
 
 (provide 'sw-center)
